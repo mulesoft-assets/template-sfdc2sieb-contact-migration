bd2b374151405e259f6f830b9c942aedcab9989c
Vladimir Andoga <vladimir.andoga@hotovo.org>
2015-05-20 09:41:50 +0200

    Updated common properties and documentation. Fixed typos and imports for log4j v2.

--- src/test/java/org/mule/templates/AbstractTemplateTestCase.java
+++ src/test/java/org/mule/templates/AbstractTemplateTestCase.java
@@ -11,8 +11,8 @@ import java.io.FileInputStream;
 import java.util.Date;
 import java.util.Properties;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Rule;
 import org.mule.api.config.MuleProperties;
 import org.mule.context.notification.NotificationException;
@@ -20,7 +20,6 @@ import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 import org.mule.tck.probe.PollingProber;
 import org.mule.tck.probe.Prober;
-import org.mule.templates.integration.BusinessLogicIT;
 import org.mule.templates.test.utils.ListenerProbe;
 import org.mule.templates.test.utils.PipelineSynchronizeListener;
 
